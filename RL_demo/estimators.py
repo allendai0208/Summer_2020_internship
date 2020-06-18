0bb5b88dd1b5812a19593a1ec2f109dd8e84eb02
Allen Dai <allendai@princeton.edu>
2020-06-18 14:25:15 -0500

    RL_demo_v1

---
+++ RL_demo/estimators.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+class Evaluation:
+    def __init__(self, horizon, num_target_policies, num_actions, target_policies):
+        """
+        inputs
+        :param horizon: size of horizon (number of requests)
+        :param num_target_policies: number of target policies
+        :param num_actions: number of actions avilable (number of servers)
+        """
+        self.horizon = horizon
+        self.num_target_policies = num_target_policies
+        self.num_actions = num_actions
+        self.num_trajectories = 0 # number of trajectories received so far
+        self.target_policies = target_policies
+
+        self.rewards = [] # Holds the per-step rewards for each trajectory
+        self.per_step_importance_ratios = [] # Holds the per-step importance ratios of each action for each policy for each trajectory
+        self.avg_cumulative_importance_ratios = [np.zeros(self.horizon) for _ in range(self.num_target_policies)]
+
+    """
+    Implement IS, stepwise IS, WIS, stepwise WIS
+    """
+    def evaluate_one_trajectory(self, trajectory):
+        self.num_trajectories += 1
+
+        trajectory_per_step_importance_ratios = [[] for _ in range(self.num_target_policies)]
+
+        for policy_index in range(self.num_target_policies):
+            policy = self.target_policies[policy_index][1]
+            num_action = 1
+
+            # sample[0] is an array of the server_load and (action, reward) revealed by each time step
+            # sample[1] is the action taken at each time step
+            # sample[2] is the reward at each time step
+            # sample[3] is an array of action probabilities in each time step
+            for sample in trajectory:
+                action = sample[1]
+                reward = sample[2]
+
+                importance_ratio = policy.choose_action(context=sample[0][0], return_prob=True, behavior_policy=False, chosen_action=action)[action] / sample[3][action]               
+                trajectory_per_step_importance_ratios[policy_index].append(importance_ratio)
+            
+                self.avg_cumulative_importance_ratios[policy_index][num_action - 1] = \
+                    (self.avg_cumulative_importance_ratios[policy_index][num_action-1] * (self.num_trajectories - 1) + np.prod(trajectory_per_step_importance_ratios[policy_index])) / self.num_trajectories
+                
+                num_action += 1
+                policy.update(action, reward) 
+
+            policy.reset()
+        
+        self.per_step_importance_ratios.append(trajectory_per_step_importance_ratios)
+        self.rewards.append([row[2] for row in trajectory])
+
+        return 
+
+
+    def get_IS_estimates(self, policy_index):
+        sum_rewards_by_trajectory = np.sum(self.rewards, axis = 1)
+        product_rhos_by_trajectory = np.prod([row[policy_index] for row in self.per_step_importance_ratios], axis=1)
+        IS_estimate_by_trajectory = product_rhos_by_trajectory * sum_rewards_by_trajectory
+        return np.mean(IS_estimate_by_trajectory)
+
+    def get_rolling_product(self, A):
+        prod = 1
+        B = []
+        for elem in A:
+            prod *= elem
+            B.append(prod)
+        return B
+
+    def get_stepIS_estimates(self, policy_index):
+        new_per_step_importance_ratios = [self.get_rolling_product(row[policy_index]) for row in self.per_step_importance_ratios]
+        stepIS_estimate_by_trajectory = [np.dot(self.rewards[t], new_per_step_importance_ratios[t]) for t in range(len(self.rewards))]
+        return np.mean(stepIS_estimate_by_trajectory)
+
+    
+    def get_WIS_estimates(self, policy_index):
+        sum_rewards_by_trajectory = np.sum(self.rewards, axis = 1)
+        product_rhos_by_trajectory = np.prod([row[policy_index] for row in self.per_step_importance_ratios], axis=1)
+        WIS_estimate_by_trajectory = np.zeros(len(sum_rewards_by_trajectory))
+        if (self.avg_cumulative_importance_ratios[policy_index][-1]):
+            WIS_estimate_by_trajectory = product_rhos_by_trajectory / self.avg_cumulative_importance_ratios[policy_index][-1] * sum_rewards_by_trajectory
+        return np.mean(WIS_estimate_by_trajectory)
+
+
+    def get_stepWIS_estimates(self, policy_index):
+        new_per_step_importance_ratios = [self.get_rolling_product(row[policy_index]) for row in self.per_step_importance_ratios]
+        new_importance_weights = [np.divide(new_per_step_importance_ratios[t], self.avg_cumulative_importance_ratios[policy_index], \
+                                            out=np.zeros_like(new_per_step_importance_ratios[t]), where=self.avg_cumulative_importance_ratios[policy_index]!=0) 
+                                            for t in range(len(self.rewards))]
+        stepWIS_estimate_by_trajectory = [np.dot(self.rewards[t], new_importance_weights[t]) for t in range(len(self.rewards))]
+        return np.mean(stepWIS_estimate_by_trajectory)
+    
+
--- RL_demo/evaluation.py
+++ RL_demo/evaluation.py
@@ -4,58 +4,65 @@ from policies import *
 from environment import Servers
 from estimators import Evaluation
 import sys
+import matplotlib.pyplot as plt
+import math
+from matplotlib.transforms import ScaledTranslation
 
 if __name__ == "__main__":
     # Initialize basic variables
-    num_seeds = 5#int(input("num_seeds: "))
-    num_requests = 15#int(input("num_requests: "))
-    num_servers = 4#int(input("num_servers: "))
+    num_seeds = 5     #int(input("num_seeds: "))
+    num_requests = 15 #int(input("num_requests: "))
+    num_servers = 4   #int(input("num_servers: "))
     threshold = int(sys.argv[1])#int(input("threshold: "))
-    num_trajectories_list = [0] + [10 ** i for i in range(1, 7)]
+    # num_trajectories_list = [0] + [10 ** i for i in range(1, 7)]
+    num_trajectories_list = [0] + [10 ** i for i in range(1, 6)]
     start_trajectory_index = 0
     # end_trajectory_index = 1
-    noise_type = 'increasing_variance'
+    #noise_type = 'increasing_variance'
 
     # Initialize policies
-    behavior_policy = ('EpsilonGreedy', EpsilonGreedy(num_servers, epsilon=0.3))
-
+    behavior_policy = ('UniRan', UniRan(num_servers))
     target_policy1 = ('LeastLoad_0.3', LeastLoad(num_servers, epsilon=0.3))
     target_policy2 = ('LeastLoad_0', LeastLoad(num_servers, epsilon=0))
-    target_policy3 = ('NonstationaryBandit', NonstationaryBandit(num_servers, 0.2, 0.2))
+    target_policy3 = ('EpsilonGreedy', EpsilonGreedy(num_servers, epsilon=0.3))
+    target_policy4 = ('UCB1', UCB1(num_servers))
 
-    target_policies = [target_policy1, target_policy2, target_policy3]
+    # behavior_policy = ('EpsilonGreedy', EpsilonGreedy(num_servers, epsilon=0.3))
+    # target_policy1 = ('LeastLoad_0.3', LeastLoad(num_servers, epsilon=0.3))
+    # target_policy2 = ('LeastLoad_0', LeastLoad(num_servers, epsilon=0))
+    # target_policy3 = ('UniRan', UniRan(num_servers))
+    # target_policy4 = ('UCB1', UCB1(num_servers))
+
+
+    target_policies = [target_policy1, target_policy2, target_policy3, target_policy4]
     num_target_policies = len(target_policies)
 
-    policies = [target_policy1, target_policy2, target_policy3, behavior_policy]
-
-    # target_policy1 = ('UCB', UCB1(num_servers, reset_period=40))
-    # target_policy2 = ('GradBandit', GradBandit(num_servers, 0.1, num_requests, num_requests + 2))
-    # target_policy3 = ('NonstationaryBandit', NonstationaryBandit(num_servers, 0.2, 0.2))
-    # target_policy4 = ('LeastLoad_0.3', LeastLoad(num_servers, epsilon=0.3))
-    # target_policy5 = ('LeastLoad_0', LeastLoad(num_servers, epsilon=0))
-    #
-    # target_policies = [target_policy1, target_policy2, target_policy3, target_policy4, target_policy5]
-    # num_target_policies = len(target_policies)
-    #
-    # policies = [target_policy1, target_policy2, target_policy3, target_policy4, target_policy5, behavior_policy]
+    policies = [target_policy1, target_policy2, target_policy3, target_policy4, behavior_policy]
     num_policies = 1 + num_target_policies
 
     target_policy_names = [policy[0] for policy in policies if policy != policies[-1]]
 
     # Initialize environment (servers)
-    servers = Servers(num_policies=num_policies, num_servers=num_servers, noise_type=noise_type)
+    servers = Servers(num_policies=num_policies, num_servers=num_servers)
 
     # Initialize evaluation for each seed
     evaluations = [Evaluation(horizon=num_requests,
                               num_target_policies=num_target_policies,
                               num_actions=num_servers,
-                              noise_type=noise_type,
-                              threshold=threshold)
+                              target_policies=target_policies)
                    for _ in range(num_seeds)]
 
     # Write output to a file
     file_name = f"num_requests_{num_requests}_num_servers_{num_servers}_threshold_{threshold}_no_noise_change.txt" #input("Enter file name: ")
     with open(file_name, 'w') as wf:
+        
+        IS_errors = [[[] for _ in range(len(num_trajectories_list) - 1)] for _ in range(num_seeds)]   
+        stepIS_errors = [[[] for _ in range(len(num_trajectories_list) - 1)] for _ in range(num_seeds)]   
+        WIS_errors = [[[] for _ in range(len(num_trajectories_list) - 1)] for _ in range(num_seeds)]   
+        stepWIS_errors = [[[] for _ in range(len(num_trajectories_list) - 1)] for _ in range(num_seeds)]   
+
+        cumulative_latency_per_policy = np.zeros(num_target_policies)
+
         # Iterate over number of trajectories
         for num_trajectories_index in range(1, len(num_trajectories_list)):
             write_string = f"\nnum_seeds: {num_seeds}" + f"\nnum_trajectory: {num_trajectories_list[num_trajectories_index]}" \
@@ -65,8 +72,9 @@ if __name__ == "__main__":
             print(write_string)
             wf.write(write_string)
 
+
             # Iterate over random seed
-            seed_add = 980918
+            seed_add = 0
             for seed in range(seed_add, seed_add + num_seeds):
                 # Set a numpy random seed
                 np.random.seed(seed)
@@ -74,64 +82,93 @@ if __name__ == "__main__":
                 # Set a particular evaluation for the seed
                 seed_evaluation = evaluations[seed - seed_add]
 
+                # Store the estimates to calculate their errors
+                IS_estimates_per_seed = np.zeros(num_target_policies)
+                stepIS_estimates_per_seed = np.zeros(num_target_policies)
+                WIS_estimates_per_seed = np.zeros(num_target_policies)
+                stepWIS_estimates_per_seed = np.zeros(num_target_policies)
+
+                # Store the errors to plot later
+                IS_errors_per_seed = np.zeros(num_target_policies)
+                stepIS_errors_per_seed = np.zeros(num_target_policies)
+                WIS_errors_per_seed = np.zeros(num_target_policies)
+                stepWIS_errors_per_seed = np.zeros(num_target_policies)
+
+
                 # Iterate over trajectories of the given number to run simulation
                 for trajectory_index in range(num_trajectories_list[num_trajectories_index - 1], num_trajectories_list[num_trajectories_index]):
-                    print(f"seed:{seed - seed_add} / trajectory: {seed_evaluation.num_trajectories}", end='\r')
+                    # print(f"seed:{seed - seed_add} / trajectory: {seed_evaluation.num_trajectories}", end='\r')
 
                     # Run simulation and get trace & true performances of every target policy
                     total_latency_for_each_policy, trace = \
                         run_simulation(policies=policies, num_requests=num_requests, servers=servers)
 
-                    # Get the estimate for every target policy on the trajectory
-                    seed_evaluation.get_estimates(trace=trace,
-                                                  target_policies=target_policies,
-                                                  true_values=total_latency_for_each_policy)
+                    # Get the estimate of every target policy on the trajectory. 
+                    """
+                    Implement
+                    """
+                    cumulative_latency_per_policy += total_latency_for_each_policy[:-1]
+                    seed_evaluation.evaluate_one_trajectory(trace)
 
-            # After finishing iterating over every random seed,
-            # display the estimate of each target policy for the given number of trajectories
-            for seed_evaluation in evaluations:
-                seed_evaluation.evaluation()
+                for policy_index in range(num_target_policies):
+                    IS_estimates_per_seed[policy_index] = seed_evaluation.get_IS_estimates(policy_index)
+                    stepIS_estimates_per_seed[policy_index] = seed_evaluation.get_stepIS_estimates(policy_index)
+                    WIS_estimates_per_seed[policy_index] = seed_evaluation.get_WIS_estimates(policy_index)
+                    stepWIS_estimates_per_seed[policy_index] = seed_evaluation.get_stepWIS_estimates(policy_index)
 
-            for target_policy_index in range(num_target_policies):
-                True_performances = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['T']
-                                              for seed_evaluation in evaluations])
-                IPS_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['IPS']
-                                          for seed_evaluation in evaluations])
-                IS_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['IS']
-                                         for seed_evaluation in evaluations])
-                SIS_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['SIS']
-                                          for seed_evaluation in evaluations])
-                SISAM_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['SISAM']
-                                            for seed_evaluation in evaluations])
-                WIS_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['WIS']
-                                          for seed_evaluation in evaluations])
-                SWIS_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['SWIS']
-                                          for seed_evaluation in evaluations])
-                SWISAM_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['SWISAM']
-                                             for seed_evaluation in evaluations])
-                DR_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['DR']
-                                         for seed_evaluation in evaluations])
-                DRAM1_estimates = np.array([seed_evaluation.estimates[target_policy_index]['estimates']['DRAM1']
-                                            for seed_evaluation in evaluations])
+                    true_performance = cumulative_latency_per_policy[policy_index]/num_trajectories_list[num_trajectories_index]
 
-                estimates_string = format_estimates(policy_name=target_policy_names[target_policy_index],
-                                                    True_performances=True_performances,
-                                                    IPS_estimates=IPS_estimates,
-                                                    IS_estimates=IS_estimates,
-                                                    SIS_estimates=SIS_estimates,
-                                                    SISAM_estimates=SISAM_estimates,
-                                                    WIS_estimates=WIS_estimates,
-                                                    SWIS_estimates=SWIS_estimates,
-                                                    SWISAM_estimates=SWISAM_estimates,
-                                                    DR_estimates=DR_estimates,
-                                                    DRAM1_estimates=DRAM1_estimates)
-                wf.write(estimates_string)
-                print(estimates_string)
+                    IS_errors_per_seed[policy_index] = (true_performance - IS_estimates_per_seed[policy_index]) / true_performance
+                    stepIS_errors_per_seed[policy_index] = (true_performance - stepIS_estimates_per_seed[policy_index]) / true_performance
+                    WIS_errors_per_seed[policy_index] = (true_performance - WIS_estimates_per_seed[policy_index]) / true_performance
+                    stepWIS_errors_per_seed[policy_index] = (true_performance - stepWIS_estimates_per_seed[policy_index]) / true_performance
 
-            # # Reset each evaluation class for the next number of trajectories
-            # for seed_evaluation in evaluations:
-            #     seed_evaluation.reset()
+                IS_errors[seed - seed_add][num_trajectories_index - 1] = IS_errors_per_seed
+                stepIS_errors[seed - seed_add][num_trajectories_index - 1] = stepIS_errors_per_seed
+                WIS_errors[seed - seed_add][num_trajectories_index - 1] = WIS_errors_per_seed
+                stepWIS_errors[seed - seed_add][num_trajectories_index - 1] = stepWIS_errors_per_seed
+
+        # After finishing iterating over every random seed,
+        # display the estimate of each target policy for the given number of trajectories
+        
+        IS_mean_errors = np.mean(IS_errors, axis=0)
+        stepIS_mean_errors = np.mean(stepIS_errors, axis=0)
+        WIS_mean_errors = np.mean(WIS_errors, axis=0)
+        stepWIS_mean_errors = np.mean(stepWIS_errors, axis=0)
+        
+        IS_std = np.std(IS_errors, axis=0)
+        stepIS_std = np.std(stepIS_errors, axis=0)
+        WIS_std = np.std(WIS_errors, axis=0)
+        stepWIS_std = np.std(stepWIS_errors, axis=0)
+
+        x = [math.pow(10, i) for i in range(1, 6)]
+
+        for policy_index in range(num_target_policies):
+            fig, ax = plt.subplots()
+            trans1 = ax.transData + ScaledTranslation(-6/72, 0, fig.dpi_scale_trans)
+            trans2 = ax.transData + ScaledTranslation(-2/72, 0, fig.dpi_scale_trans)
+            trans3 = ax.transData + ScaledTranslation(+2/72, 0, fig.dpi_scale_trans)
+            trans4 = ax.transData + ScaledTranslation(+6/72, 0, fig.dpi_scale_trans)
+
+            ax.errorbar(x, [row[policy_index] for row in IS_mean_errors], yerr=[row[policy_index] for row in IS_std], transform=trans1, capsize=3, capthick=1, linewidth=1, marker='x', label='IS')
+
+            ax.errorbar(x, [row[policy_index] for row in stepIS_mean_errors], yerr=[row[policy_index] for row in stepIS_std], transform=trans2, capsize=3, capthick=1, linewidth=1, marker='x', label='stepIS')
+                             
+            ax.errorbar(x, [row[policy_index] for row in WIS_mean_errors], yerr=[row[policy_index] for row in WIS_std], transform=trans3, capsize=3, capthick=1, linewidth=1, marker='x', label='WIS')
+            
+            ax.errorbar(x, [row[policy_index] for row in stepWIS_mean_errors], yerr=[row[policy_index] for row in stepWIS_std], transform=trans4, capsize=3, capthick=1, linewidth=1, marker='x', label='stepWIS')
+            
+
+            plt.ylabel('Mean errors of estimators')
+            plt.xlabel('Number of samples (log10)')
+            plt.xscale('log')
+            plt.legend()
+
+            plt.show()
+
+        print(stepWIS_errors)
 
 
 
-
+                
+            
